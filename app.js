433d537a41fdc841722f6b1afcd0cbd31f75a191
matr0s <serhiy.osadchuk@gmail.com>
2017-08-27 18:46:29 +0300

    stage 1

--- app.js
+++ app.js
@@ -9,7 +9,47 @@ GAME RULES:
 
 */
 
-// test commit 2
-// test 4
-//test 5
-// tes 6
+var scores, roundScore, activePlayer, dice;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+dice = Math.floor(Math.random() * 6) + 1;
+document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';	
+var x = document.querySelector('#score-' + activePlayer).textContent;
+document.querySelector('.dice').style.display = 'none';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
