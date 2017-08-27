4d0552a0ae20a6d0aa7ebda7e57bf90b57ab3dff
matr0s <serhiy.osadchuk@gmail.com>
2017-08-27 20:53:37 +0300

    Lecture 42

--- app.js
+++ app.js
@@ -9,24 +9,62 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-document.querySelector('#current-' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';	
-var x = document.querySelector('#score-' + activePlayer).textContent;
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
+
+var diceDOM = document.querySelector('.dice');
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+	// 1. Random number
+	var dice = Math.floor(Math.random() * 6) + 1;
+	// 2. Display result
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'dice-' + dice + '.png';
+	// 3. Update Round score in rolled number not 1
+	if (dice !== 1) {
+	roundScore = document.querySelector('#current-' + activePlayer).textContent = roundScore + dice;
+	} else {
+	nextPlayer();
+	}
+	});
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+	scores[activePlayer] = document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer] + roundScore;
+	if (scores[activePlayer] >= 10) {
+		document.querySelector('#name-' + activePlayer).textContent = 'WINNER!'
+		diceDOM.style.display = 'none';
+		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');	
+		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');	
+
+		
+	} else {
+	nextPlayer();
+	}
+	});
+
+function nextPlayer() {
+	roundScore = document.querySelector('#current-' + activePlayer).textContent = 0;
+	document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');	
+	activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+	document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');	
+	diceDOM.style.display = 'none';
+}
 
 
 
-
-
-
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';	
+//var x = document.querySelector('#score-' + activePlayer).textContent;
 
 
 
